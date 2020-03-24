a728963f7f4ad0a447f4b32f66e28bdb01e0e8eb
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-03-24 17:42:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ bot.on("message", async (message) => {
 });
 
 function playMusic(message, connection){
+  console.log(connection);
   var dispatcher = connection.playStream(ytdl(musicQueue[0], {filter: "audioonly"}));
 
   musicQueue.shift();
